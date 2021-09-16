8f81b5787d7d0911d3795aa639ec4112ca86385f
Le Chau Kha <lechaukha@Les-MacBook-Pro.local>
2021-09-17 00:25:48 +0700

    First

---
+++ Node_exporter_installer.sh
@@ -0,0 +1,35 @@
+#!bin/bash
+
+# Tao User
+useradd --no-create-home --shell /bin/false node_exporter
+# Install
+cd /opt
+wget https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-amd64.tar.gz
+
+tar xvf node_exporter-0.18.1.linux-amd64.tar.gz
+
+cp node_exporter-0.18.1.linux-amd64/node_exporter /usr/local/bin
+chown node_exporter:node_exporter /usr/local/bin/node_exporter
+
+rm -rf node_exporter-0.18.1.linux-amd64*
+cd -
+#Tao service systemd
+cat <<EOF >  /etc/systemd/system/node_exporter.service
+[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=multi-user.target
+EOF
+#systemd
+systemctl daemon-reload
+systemctl start node_exporter
+systemctl enable node_exporter
